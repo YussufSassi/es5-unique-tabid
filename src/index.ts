343728a54dc73f18e4328e86ba882a0f705ca9ec
Yussuf Sassi <yussuf.sassi@krankikom.biz>
2024-05-13 15:00:36 +0200

    change ts target to ES5 and adjust used language features

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 export default class UniqueTabId {
-  #sessionStorageKey: string;
-  #sessionStorageKey_parentTab: string;
-  #gotResponse = false;
-  #channel: BroadcastChannel | null  = null;
+  sessionStorageKey: string;
+  sessionStorageKey_parentTab: string;
+  gotResponse = false;
+  channel: BroadcastChannel | null  = null;
 
   /**
    * A callback that is called with the tabId, isTabNew and parentTabId, after the Broadcast Channel communication is done
@@ -22,28 +22,28 @@ export default class UniqueTabId {
     this.initTab = this.initTab.bind(this);
     this.removeTabId = this.removeTabId.bind(this);
 
-    this.#sessionStorageKey = `unique-tabid-${appId}`;
-    this.#sessionStorageKey_parentTab = `unique-tabid-${appId}_parent`;
-    this.#channel = new BroadcastChannel(`unique-tabid-broadcast-channel-${appId}`);
-    this.#channel.onmessage = (msg) => {
+    this.sessionStorageKey = `unique-tabid-${appId}`;
+    this.sessionStorageKey_parentTab = `unique-tabid-${appId}_parent`;
+    this.channel = new BroadcastChannel(`unique-tabid-broadcast-channel-${appId}`);
+    this.channel.onmessage = (msg) => {
       const type = msg && msg.data && msg.data.type;
       let tabId = msg && msg.data && msg.data.tabId;
     
       if(type === "SEARCH"){
-        if(window.sessionStorage.getItem(this.#sessionStorageKey) === tabId){
-          this.#channel!.postMessage({
+        if(window.sessionStorage.getItem(this.sessionStorageKey) === tabId){
+          this.channel!.postMessage({
             type: "FOUND",
             tabId
           })
         }
       }
       else if(type === "FOUND"){
-        if(window.sessionStorage.getItem(this.#sessionStorageKey) === tabId){
-          this.#gotResponse = true;
+        if(window.sessionStorage.getItem(this.sessionStorageKey) === tabId){
+          this.gotResponse = true;
           const parentTabId = tabId;
           tabId = this.uniqIdFunc();
-          window.sessionStorage.setItem(this.#sessionStorageKey_parentTab, parentTabId);
-          window.sessionStorage.setItem(this.#sessionStorageKey, tabId);
+          window.sessionStorage.setItem(this.sessionStorageKey_parentTab, parentTabId);
+          window.sessionStorage.setItem(this.sessionStorageKey, tabId);
           this.tabIdCallback({tabId, isTabNew: true, parentTabId});
         }
       }
@@ -52,25 +52,25 @@ export default class UniqueTabId {
 
 
   removeTabId(){
-    window.sessionStorage.removeItem(this.#sessionStorageKey);
+    window.sessionStorage.removeItem(this.sessionStorageKey);
   }
   
   initTab(){
-    let tabId = window.sessionStorage.getItem(this.#sessionStorageKey);
+    let tabId = window.sessionStorage.getItem(this.sessionStorageKey);
     if(tabId === null){
       tabId = this.uniqIdFunc();
-      window.sessionStorage.setItem(this.#sessionStorageKey, tabId);
+      window.sessionStorage.setItem(this.sessionStorageKey, tabId);
       this.tabIdCallback({tabId, isTabNew: true, parentTabId: null});
     }
     else{
-      this.#gotResponse = false;
-      this.#channel!.postMessage({
+      this.gotResponse = false;
+      this.channel!.postMessage({
         type: "SEARCH",
         tabId
       });
       setTimeout(() => {
-        if(!this.#gotResponse){
-          const parentTabId = window.sessionStorage.getItem(this.#sessionStorageKey_parentTab);
+        if(!this.gotResponse){
+          const parentTabId = window.sessionStorage.getItem(this.sessionStorageKey_parentTab);
           this.tabIdCallback({tabId: tabId!, isTabNew: false, parentTabId});
         }
       }, this.WAIT_TIMEOUT);
